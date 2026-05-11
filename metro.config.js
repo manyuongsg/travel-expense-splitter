1ba95a25cf5af770ff61eb89ff5fa43e7d33e325
omanyu <manyuong.sg@gmail.com>
2026-05-11 10:20:05 +0800

    minor updates

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.maxWorkers = 1;
+// Limit workers on Windows to avoid EBUSY file-locking errors
+if (process.platform === 'win32') {
+  config.maxWorkers = 1;
+}
 
 module.exports = config;
